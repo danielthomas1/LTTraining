b4dc06df2df5634ed2414e4e50a7568a8e98d104
aranghat <58101741+aranghat@users.noreply.github.com>
2021-12-17 15:47:01 +0530

    Added Data Types

---
+++ DataTypes/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace DataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Numerical
+            sbyte smallByte = 0;//8bit signed integer -128 to 127
+            byte byteType   = 0;//a bit unsigned integer 0-255
+
+            Console.WriteLine(sbyte.MinValue + "  " + sbyte.MaxValue);
+            Console.WriteLine(byte.MinValue  + "  " + byte.MaxValue);
+
+            short shortDataType     = 0;//16 bit signed integer
+            ushort uShortDataType   = 0;//16 bit unsigned integer
+
+            Console.WriteLine(short.MinValue + "  " + short.MaxValue);
+            Console.WriteLine(ushort.MinValue + "  " + ushort.MaxValue);
+
+            int  intDataType = 0;//32 bit signed integer
+            uint uIntDataType = 0;//32 bit unsigned integer
+
+            Console.WriteLine(int.MinValue + "  " + int.MaxValue);
+            Console.WriteLine(uint.MinValue + "  " + uint.MaxValue);
+
+            long  longDataType    = 0;//64 bit signed integer
+            ulong ulongDataType   = 0;//64 bit unsigned integer
+
+            Console.WriteLine(long.MinValue + "  " + long.MaxValue);
+            Console.WriteLine(ulong.MinValue + "  " + ulong.MaxValue);
+
+            //Decimal Places
+            float floatDataType = 0.1234567890F; //4byte long signed value with 6-9 decimal places;
+            Console.WriteLine(floatDataType);
+
+            double doubleType = 0.123456789123456789; //8byte long signed value 15-17 decimal places
+            Console.WriteLine(doubleType);
+
+            decimal decimalType = 0.123456789123456789123456789M; //16byte long signed value 28-29 decimal places
+            Console.WriteLine(decimalType);
+
+            //String
+            string str  = "This is a long string message";
+
+            //Char
+            char ch     = 'C';
+
+            //Boolean
+            bool isActive = true;
+            bool isOnline = false;
+        }
+    }
+}
---
+++ SimpleDataTypes/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace SimpleDataTypes
+{
+    class Program
+    {
+        static void Main()
+        {
+            /*
+            Console.Write("Enter the first number :");
+            string strFirstNumber = Console.ReadLine();
+
+            Console.Write("Enter the second number :");
+            string strSecondNumber = Console.ReadLine();
+
+            float totalTax = .30F;
+
+            int firstNumber;
+            int secondNumber;
+
+            if (int.TryParse(strFirstNumber, out firstNumber)
+                && int.TryParse(strSecondNumber, out secondNumber))
+            {
+                Console.WriteLine($"{firstNumber} + {secondNumber} = {firstNumber + secondNumber}");
+            }
+            else
+                Console.WriteLine("One or more data is not a number");*/
+
+            Console.Write("Enter your annual salary : ");
+            string strAnnualSalary = Console.ReadLine();
+            
+            int annualSalary;
+            if (int.TryParse(strAnnualSalary, out annualSalary))
+            {
+                float tax = 0.0F;
+
+                if (annualSalary >= 200000 && annualSalary < 500000)
+                    tax = .10F;
+                else if (annualSalary >= 500000 && annualSalary < 700000)
+                    tax = .20F;
+                else if (annualSalary >= 700000)
+                    tax = .30F;
+
+                Console.WriteLine($"Your tax for {annualSalary} for the year is {annualSalary * tax}");
+
+            }
+            else
+                Console.WriteLine("Please enter a valid value for the salary");
+
+        }
+    }
+}
